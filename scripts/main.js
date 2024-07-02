8af96a38dfa5ee41dfc719cbd5b1d5015bf605da
ryuukkk <thorykartik111@gmail.com>
2024-07-02 13:37:14 -0400

    Initial Commit

---
+++ scripts/main.js
@@ -0,0 +1,42 @@
+// myHeading.textContent = 'Hello world!';
+
+// document.querySelector("html").addEventListener("click", function () {
+//     alert("Ouch! Stop poking me!");
+//   });
+  
+// document.querySelector("html").onclick = function () {
+//     alert("Ouch! Stop poking me!");
+//   };
+
+const myImage = document.querySelector("img");
+
+myImage.onclick = () => {
+const mySrc = myImage.getAttribute("src");
+if (mySrc === "images/logo.png") {
+    myImage.setAttribute("alt", "Change");
+    myImage.setAttribute("src", "");
+} else {
+    myImage.setAttribute("src", "images/firefox-icon.png");
+}
+};
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+function setUserName() {
+    let myName = prompt("Please enter your name.");
+    localStorage.setItem("name", myName);
+    myHeading.textContent = "Welcome, " + myName;
+}
+
+if (!localStorage.getItem("name")) {
+    setUserName();
+}
+else {
+    let storedName = localStorage.getItem("name");
+    myHeading.textContent = "Welcome, " + storedName;
+}
+myButton.onclick = () => {
+    setUserName();
+  };
+  
